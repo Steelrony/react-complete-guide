ce0a85dcb57458c2edaa77d7f339c40de97d5bff
steelrony <uchechukwuaronu@gmail.com>
2018-10-15 10:28:28 +0100

    verify changes later

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ class App extends Component {
 
   nameChangedHandler = ( event, id ) => {
     const personIndex = this.state.persons.findIndex(p => {
-      return p.id === id;
+      return p.Userid === id;
     });
 
     const person = {
